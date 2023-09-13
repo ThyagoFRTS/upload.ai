353ccb03a1aab523aaaaeb1777a7bd1c41628a24
ThyagoFRTS <kzkr.thyago@gmail.com>
2023-09-13 17:00:58 -0300

    feat: create api

---
+++ upload-ai-api/src/server.ts
@@ -0,0 +1,19 @@
+import { fastify } from "fastify";
+import { prisma } from "./lib/prisma";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+
+const app = fastify();
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log("HTTP server runnings"));
